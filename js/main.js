adf1033e8ddb0c5bfd9544eab423cc03431f0f99
JANGYOUNGAH <jjangah10@gmail.com>
2023-03-16 17:08:57 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  // searchEl라는 함수에(클래스 써치) 클릭이라는 이벤트가 일어나면 
-  // serchInputEl라는 함수에 focus기능이 실행된다.(핸들러)
-  searchInputEl.focus();
-});
-
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  // set=지정 attribute=html속성, 써치인풋엘리먼트 부분에 html속성을 지정할 때 사용한다.
-  // placeholder=인풋요소 힌트
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -167,7 +145,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller())
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
